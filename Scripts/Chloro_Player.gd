91c8ddf1dbf9b082e78b16fd1e397f0779b73c17
zach-idkunow <zach.iduknow@gmail.com>
2022-01-17 21:05:10 -0800

    Rough grappling hook
    I just need the line renderer and a cross hair

--- Scripts/Chloro_Player.gd
+++ Scripts/Chloro_Player.gd
@@ -4,7 +4,7 @@ extends KinematicBody
 export var speed := 7.0
 export var gravity := 9.8
 export var jump_force := 5.0
-
+export var grapple_speed := 0.05
 #player movement vectors(movment is the final vector used for move_and_slide())
 var movement = Vector3()
 #keeps track of player direction
@@ -18,14 +18,21 @@ var gravity_vector = Vector3()
 export var mouse_sensitivity := 0.2
 #acceleration to max speed when player is moving
 export var default_acceleration = 7
+#slower acceleration means the play comes to a stop slower(good for in air travel)
 export var air_acceleration = 1
 onready var acceleration = default_acceleration
 #used for standing on slopes
 var snap
+var grappling = false
+var hookpoint = Vector3()
+var hookpoint_get = false
+var hook_dist = Vector2()
+var point_dist = Vector2()
 
 #player components
 onready var head = $Head
 onready var camera = $Head/Camera
+onready var grapple_cast = $Head/Camera/GrappleCast
 
 func _ready():
 	#locks mouse to center of the screen
@@ -37,8 +44,8 @@ func _input(event):
 		head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
 		head.rotation.x = clamp(head.rotation.x, deg2rad(-89.0), deg2rad(89.0))
 
-	
 func _physics_process(delta):
+	grapple()
 	#resetting the direction vector every frame
 	direction = Vector3.ZERO
 	#gets whatever current y rotation we have from the basis matrix
@@ -53,15 +60,18 @@ func _physics_process(delta):
 		snap = -get_floor_normal()
 		#reseting the acceleration gained from falling
 		gravity_vector = Vector3.ZERO
-	else:
+		acceleration = default_acceleration
+	elif not is_on_floor():
 		#only one direction to stick to
 		snap = Vector3.DOWN
+		acceleration = air_acceleration
 		#adding the acceleration from gravity
 		gravity_vector += Vector3.DOWN * gravity * delta
 	
 	#I can eventually add a variable for double jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		snap = Vector3.ZERO
+		
 		gravity_vector = Vector3.UP * jump_force
 	
 	#makes for smooth movement by interpolating from whatever the current direction is times speed by delta
@@ -69,3 +79,30 @@ func _physics_process(delta):
 	movement = velocity + gravity_vector
 	
 	move_and_slide_with_snap(movement,snap,Vector3.UP)
+
+func grapple():
+	#getting global transform as vector2 to stop sticking
+	point_dist = Vector2(transform.origin.x, transform.origin.z)
+	#checks to see if player pushed grapple and sets grappling true
+	if Input.is_action_just_pressed("grapple"):
+		if grapple_cast.is_colliding():
+			if not grappling:
+				grappling = true
+	if grappling:
+		#turns off gravity
+		gravity_vector = Vector3()
+		#set the target based on where the raycast collided
+		if not hookpoint_get:
+			hookpoint = grapple_cast.get_collision_point()
+			hookpoint_get = true
+			hook_dist = Vector2(hookpoint.x,hookpoint.z)
+		#sees if the grapple point is at least 1 unit away from player
+		
+		if hook_dist.distance_to(point_dist) > 1.5:
+			if hookpoint_get:
+				#transform.origin = lerp(transform.origin, hookpoint, grapple_speed)
+				print(hook_dist.distance_to(point_dist))
+				transform.origin = lerp(transform.origin,hookpoint,grapple_speed)
+		else:
+			grappling = false
+			hookpoint_get = false
